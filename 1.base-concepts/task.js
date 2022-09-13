ba7b0c0405801ea9d0c05ba1c161037f01a39413
AnfimovSergey <107045150+AnfimovSergey@users.noreply.github.com>
2022-09-13 13:49:14 +0300

    Update task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,8 +1,28 @@
-function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+"use strict";
+function  (a, b, c)  {
+  let x1, x2, disc = Math.pow(b,2)-4*a*c;
+  let arr = [];
+
+  if  (disc < 0) return arr;
+
+  if  (disc > 0) {
+    x2 = (b*(-1) + Math.sqrt(disc))/2*a;
+    arr.push(x2);
+  }
+  x1 = (b*(-1) - Math.sqrt(disc))/2*a;
+  arr.push(x1);
+
+  return arr;
 }
+
+
+
+
+
+
+
+
+
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
